f1dcd0d8c9da1d1989bf0b1bcc93fe2ad2e130e0
Andrew Chang-DeWitt <11323923+andrew-chang-dewitt@users.noreply.github.com>
2024-10-02 17:10:58 -0500

    Add prompt as doc comment.

--- task1.s
+++ task1.s
@@ -1,3 +1,21 @@
+## Task 1: Swap elements in an array (8 points)
+##
+## Please declare an array A in your data segment
+## with 10 random integers. Program to find the
+## maximum number of A and put it as the last
+## element in the array while other elements
+## relative order does not change. Then print A[0] 
+## to A[9] on the screen.
+##
+## For example:
+## - Input: A=(2,-1,3,8,10,5,4,23,-20,6)
+## - Output: A=(2,-1,3,8,10,5,4,-20,6,23)
+##
+## Test your program by giving different values
+## to A. Please save the screenshot of the two
+## following test cases:
+## 1. A=(7,12,3,6,23,90,-2,-122,10,1)
+## 2. A = (1205,5523,703,66,-324,0,-9,80,5048,990)
 	.data
 A:	.word 2, -1, 3, 8, 10, 5, 4, 23, -20, 6
 	# max_last(A) => mutate A to be 2, -1, 3, 8, 10, 5, 4, -20, 6, 23
@@ -22,12 +40,12 @@ main: 	# max_last(A) is
 	# swap a_n-1 & a_mi
 
 	# i <- 0 reset loop counter
-  # print every array
-  # loop from i to n-1:
+	# print every array
+	# loop from i to n-1:
 	#   arg? <- val in a_i
-  #   arg? <- print code
-  #   exec print
-  # end loop
+	#   arg? <- print code
+	#   exec print
+	# end loop
 
 exit:
 	addi a0, x0, 10		# set up exit call
