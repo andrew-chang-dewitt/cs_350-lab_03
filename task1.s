86a8738bea026c3edeba6d69ed1b8615965e7a42
Andrew Chang-DeWitt <11323923+andrew-chang-dewitt@users.noreply.github.com>
2024-10-02 17:45:48 -0500

    Incomplete first pass at translating pseudocode.

--- task1.s
+++ task1.s
@@ -21,26 +21,30 @@ A:	.word 2, -1, 3, 8, 10, 5, 4, 23, -20, 6
 	# max_last(A) => mutate A to be 2, -1, 3, 8, 10, 5, 4, -20, 6, 23
 	# TODO: add test cases from assignment here
 	.text
-main: 	# max_last(A) is
+main:	# max_last(A) is
 	#   input:       A <- unordered array of unsigned ints
 	#                     in terms of a_i s.t. i is in [0..9]
 	#   output:      None
 	#   side-effect: swap last term in A w/ max term in A
-	# n  <- length of A, 10
-	# &A <- pointer to A[0]
-	# ma <- max value, init as a_0
-	# mi <- max index, init as 0
-	# i  <- temp value track current loop iteration (1..n-1), init @ 1
-	# skip first value as it's already stored in max
-	# loop from i to n-1: 
-	#   if mv < a_i:
+	addi t0 x0 40 # n  <- length of A in bytes, 10 * 4
+	la t1 A       # &A <- pointer to A[0]
+	lw t2 0(t1)   # mv <- max value, init as a_0
+	add t3 x0 x0  # mi <- max index, init as 0
+	addi t4 x0 4  # i  <- temp value track current loop iteration (1..n-1), 
+	               # init i @ 1 byte to skip first value as it's already in max
+loop:	# loop from i to n-1: 
+	blt a0 mv endif #   if mv < a_i:
 	#     mv <- a_i
 	#     mi <- i
+endif:	# end loop iteration
+	addi t4 t4 1    # i++
+	blt t4 t0 loop  # check if i < n
 	# end loop
 	# swap a_n-1 & a_mi
+	# tmp <- a_n-1
 
 	# i <- 0 reset loop counter
-	# print every array
+print:	# print every array
 	# loop from i to n-1:
 	#   arg? <- val in a_i
 	#   arg? <- print code
