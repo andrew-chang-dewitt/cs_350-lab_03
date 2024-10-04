ceded1c6c5b19b6b0710a64d3a7c56df0896841b
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-10-03 22:08:37 -0500

    First example solved.

--- task1/main.s
+++ task1/main.s
@@ -15,49 +15,59 @@
 ## to A. Please save the screenshot of the two
 ## following test cases:
 ## 1. A=(7,12,3,6,23,90,-2,-122,10,1)
-## 2. A = (1205,5523,703,66,-324,0,-9,80,5048,990)
+## 2. A=(1205,5523,703,66,-324,0,-9,80,5048,990)
 	.data
 A:	.word 2, -1, 3, 8, 10, 5, 4, 23, -20, 6
 comma:  .string ", "
 	# max_last(A) => mutate A to be 2, -1, 3, 8, 10, 5, 4, -20, 6, 23
 	# TODO: add test cases from assignment here
+
 	.text
-main:	# # max_last(A) is
-	# #   input:       A <- unordered array of unsigned ints
+
+main:	la s0 A         # &A <- pointer to A[0]
+
+	# # max_last(A) is
+	# #   input:       A <- ptr to unordered array of unsigned ints
 	# #                     in terms of a_i s.t. i is in [0..9]
 	# #   output:      None
 	# #   side-effect: swap last term in A w/ max term in A
-	li s0 36        # n - 1 <- length of A in bytes, (10 - 1) * 4
-	la s1 A         # &A <- pointer to A[0]
+	addi s1 s0 36   # ptr to A[n-1] to know when to stop processing A
+	lw s2 0(s0)     # max value so far <- a_1
+	add s3 s0 x0    # addr of max so far <- addr a_1
+
+	# set up to loop over A
+	addi s4 s0 4    # i  <- temp value track current loop iteration (1..n-1),
+
+loop:	# from i to n-1:
+	lw t1 0(s4)     # get a_i
+	blt t1 s2 endif # if a_i < max, do nothing
+	add s2 t1 x0    # else update max to a_i
+	add s3 s4 x0    # and max index to addr of a_i
+	# then prepare to loop again
+endif:	addi s4 s4 4    # i++
+	blt s4 s1 loop  # loop again if i still < n-1
+	# else end loop
+
+	# move max to end, shifting everything else down 1 word
+move:   # j <- max value addr; used as loop counter since it already
+	# points to addr to start processing at
+	lw t1 4(s3)     # get value 1 past current addr
+	sw t1 0(s3)     # and store it in current addr
+	addi s3 s3 4    # j++
+	blt s3 s1 move  # loop again if j < n-1
+	# else end loop
+	sw s2 0(s1)     # move max to a_n-1
+
+	# prepare to print results
 	la s2 comma    # &A <- pointer to A[0]
-	# lw a0 0(s1)   # mv <- max value, init as a0
-	# add t3 x0 x0  # mi <- max index, init as 0
-	# addi t4 x0 4  # i  <- temp value track current loop iteration (1..n-1),
-	#               # init i @ 1 byte to skip first value as it's already in max
-
-loop:	# # loop from i to n-1:
-	# blt a0 t2 endif #   if mv < a_i:
-	# #     mv <- a_i
-	# #     mi <- i
-
-endif:	# # end loop iteration
-	# addi t4 t4 1    # i++
-	# blt t4 s0 loop  # check if i < n
-	# # end loop
-	# # swap a_n-1 & a_mi
-	# # tmp <- a_n-1
-
-	li t4 0         # i <- 0 reset loop counter
+	add s4 s0 x0    # i <- 0 reset loop counter
 print:	# print every array entry
-	                # loop from i to n-1:
-	add t5 t4 s1    # get pointer to i by adding loop counter to a0 location
-	lw a0 0(t5)     #   arg0 <- val in a_i
+			# loop from i to n-1:
+	lw a0 0(s4)     #   arg0 <- val in a_i
 	li a7 1         #   arg7 <- print code
 	ecall           #   exec print
-	# FIXME: somehow we're printing 12 elements instead of 10?
-	#        check math on incrementing i, maybe add a print statement for it
-	addi t4 t4 4    # i += 4 to increment by word
-	blt s0 t4 exit  # check if loop keeps printing, if so:
+	addi s4 s4 4    # i += 4 to increment by word
+	blt s1 s4 exit  # if i < n, loop again, else goto exit
 	add a0 s2 x0    # print array item delimiter
 	li a7 4
 	ecall
