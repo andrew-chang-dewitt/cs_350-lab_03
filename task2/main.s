97618c780414c5580cce7ccd52b08a35abe14d04
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-10-04 13:44:12 -0500

    Solve example.

--- task2/main.s
+++ task2/main.s
@@ -22,6 +22,10 @@
 ## cases below.
 ##   1) A=(10,20,30,40), B=(90,80,70,60,50)
 ##   2) A=(3,2,1,0,1,2,3), B=(7,8,9,10,9,8,7)
+# 	.include "debug.s"
+
+## --- BEGIN PROGRAM ---
+	.globl main
 
 	.data
 	# Example: A=(2, 4, 6), B=(1,3)
@@ -30,7 +34,6 @@ A:	.word 2, 4, 6
 B:	.word 1, 3
 x:	.word 3
 y:	.word 2
-z:	.word 3
 C:	.word 0, 0, 0 # init C as empty
 	# test cases from prompt:
 	# 1) A=(10,20,30,40), B=(90,80,70,60,50)
@@ -39,7 +42,6 @@ C:	.word 0, 0, 0 # init C as empty
 # B:	.word 90, 80, 70, 60, 50
 # x:	.word 4
 # y:	.word 5
-# z:	.word 5
 # C:	.word 0, 0, 0, 0, 0 # init C as empty
 	# 2) A=(3,2,1,0,1,2,3), B=(7,8,9,10,9,8,7)
 	#    sum_arr(A, B, x, y) => C=(10,10,10,10,10,10,10)
@@ -47,7 +49,6 @@ C:	.word 0, 0, 0 # init C as empty
 # B:	.word 7, 8, 9, 10, 9, 8, 7
 # x:	.word 7
 # y:	.word 7
-# z:	.word 7
 # C:	.word 0, 0, 0, 0, 0, 0, 0 # init C as empty
 comma:  .string ", "
 colon:  .string ": "
@@ -64,10 +65,13 @@ main:
 	la t0 y          # &y
 	lw a3 0(t0)      # y <- *t0
 	jal ra sum_arr
+# 	debug("got arr ptr", a0, 34)
+# 	debug("got arr len", a1, 1)
+# 	debug("passing arr ptr to print", a0, 34)
+# 	debug("passing arr len to print", a1, 1)
 
 	# C already in position from sum_arr return for printing results
-	# FIXME: uncomment next line when sum_arr is done
-	# jal ra print_arr
+	jal ra print_arr
 
 	j exit
 # END main
@@ -85,49 +89,70 @@ sum_arr:
 	sw ra 0(sp)
 
 	# BODY:
-	bge a3 a1 sa_b_ge # is a or b longer?
-	add t0 a0 x0 # t0 <- ptr for indexing longer array
-	add t1 a2 x0 # t1 <- ptr for indexing shorter (or equal) array
-	la t2 C # t2 <- ptr for indexing result array
+	bge a3 a1 sa_b_ge # if a is longer:
+# 	debug_msg("A is longer than B")
+	add s0 a0 x0 # s0 <- ptr for indexing longer array
+	add s1 a2 x0 # s1 <- ptr for indexing shorter (or equal) array
+	la s2 C # s2 <- ptr for indexing result array
+	add s5 s2 x0 # save original C ptr for return value
+	add s6 a1 x0 # save longer distance as distance of result array
 	slli a1 a1 2
-	add t3 a1 t0 # t3 <- t0 + longer distance
+	add s3 a1 s0 # s3 <- end of longer array
 	slli a3 a3 2
-	add t4 a3 t0 # t4 <- t0 + shorter distance
+	add s4 a3 s0 # s4 <- end of shorter array
 	j sa_sum_loop
 
-sa_b_ge:
-	add t0 a2 x0 # t0 <- ptr for indexing longer array
-	add t1 a0 x0 # t1 <- ptr for indexing shorter (or equal) array
-	la t2 C # t2 <- ptr for indexing result array
+sa_b_ge:	     # else b is longer
+# 	debug_msg("B is longer than A")
+	add s0 a2 x0 # s0 <- ptr for indexing longer array
+	add s1 a0 x0 # s1 <- ptr for indexing shorter (or equal) array
+	la s2 C # s2 <- ptr for indexing result array
+	add s5 s2 x0 # save original C ptr for return value
+	add s6 a3 x0 # save longer distance as distance of result array
 	slli a3 a3 2
-	add t3 a3 t0 # t3 <- t0 + longer distance
+	add s3 a3 s0 # s3 <- end of longer array
 	slli a1 a1 2
-	add t4 a1 t0 # t4 <- t0 + shorter distance
+	add s4 a1 s0 # s4 <- end of shorter array
 
 sa_sum_loop:
-	# t5 <- load t0
-	# t6 <- load t1
-	# t7 <- t5 + t6
-	# mem@t2 <- t7
-	# t0 + 4
-	# t1 + 4
-	# t2 + 4
-	# if t3 < t0, goto rest loop
-	# else, go again
+# 	debug("for i", s0, 34)
+	lw t0 0(s0)  # t0 <- load s0
+# 	debug("a_i", t0, 1)
+	lw t1 0(s1)  # t1 <- load s1
+# 	debug("b_i", t1, 1)
+	add t2 t1 t0 # t2 <- t0 + t1
+	sw t2 0(s2)  # mem@s2 <- t2
+# 	debug("put into c_i", t2, 1)
+	addi s0 s0 4 # s0 + 4
+# 	debug("incremented i to", s0, 34)
+	addi s1 s1 4 # s1 + 4
+	addi s2 s2 4 # s2 + 4
+# 	debug("is i still less than s4", s4, 34)
+	bge s0 s4 sa_rst_loop # if s4 < s0, goto rest loop
+# 	debug_msg("yes, going again")
+	j sa_sum_loop # else, go again
 
-	# set up rest loop
 sa_rst_loop:
-	# t5 <- load t0
-	# mem@t2 <- t5
-	# t0 + 4
-	# t2 + 4
-	# if t4 < t0, goto exit
-	# else, go again
+# 	debug_msg("no, going to rest loop")
+	lw t0 0(s0)  # t0 <- load s0
+# 	debug("a_i", t0, 1)
+	sw t0 0(s2)  # mem@s2 <- t0
+# 	debug("put into c_i", t0, 1)
+	addi s0 s0 4 # s0 + 4
+# 	debug("incremented i to", s0, 34)
+	addi s2 s2 4 # s2 + 4
+# 	debug("is i still less than s3", s3, 34)
+	bge s0 s3 sa_exit # if s3 < s0, goto fn exit
+# 	debug_msg("yes, going again")
+	j sa_rst_loop # else, go again
 
 
 sa_exit:
-	# a0 <- ptr to result array
-	# a1 <- value of result array length
+# 	debug_msg("no, going to fn exit")
+	add a0 s5 x0 # a0 <- s5, ptr to result array
+	add a1 s6 x0 # a1 <- s6, value of result array length
+# 	debug("returning arr ptr", a0, 34)
+# 	debug("returning arr len", a1, 1)
 	# TEARDOWN:
 	lw ra 0(sp)
 	addi sp sp 4      # pop return addr from stack
@@ -140,9 +165,11 @@ sa_exit:
 #         a1: n <- length of A
 # output: none
 # side effect: print every array entry to stdout
-# BODY:
-# prepare to print results
 print_arr:
+	# BODY:
+	# prepare to print results
+# 	debug("given arr ptr", a0, 34)
+# 	debug("given arr len", a1, 1)
 	la t2 comma       # ptr to array element delimiter
 	la t3 nline       # ptr to newline char
 	slli a1 a1 2      # multiply length by 4 to get as words
@@ -152,6 +179,7 @@ print_arr:
 	# print results in loop
 pa_loop:
                           # loop from i to n-1:
+# 	debug("for i", t0, 34)
 	lw a0 0(t0)       # printing int value in each a_i
 	addi a7 x0 1
 	ecall
